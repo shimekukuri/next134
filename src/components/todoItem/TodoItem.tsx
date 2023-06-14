a2c79a238a0950efe7a6b89b37479ab84ede1640
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-14 10:21:16 -0400

    -

---
+++ src/components/todoItem/TodoItem.tsx
@@ -0,0 +1,24 @@
+interface TodoItemProps {
+  id: string;
+  title: string;
+  complete: boolean;
+}
+
+export default function TodoItem({ id, title, complete }: TodoItemProps) {
+  return (
+    <li className="flex gap-1 items-center">
+      <input
+        id={id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+      />
+      <label
+        htmlFor={id}
+        className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
+    </li>
+  );
+}
